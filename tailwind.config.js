e8b3d0e466b1b725d065fb7d8cd9a5ed5a7dfe03
kamransaeed66 <kamran012@gmail.com>
2022-08-23 17:41:50 +0500

    in progress

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "public/*.{html,js}",
+  ],
+  theme: {
+    colors: {
+      blue: {
+        btn_blue : '#1D7DEA',
+      }
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
